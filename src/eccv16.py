16cd7d301f07d3157819c828393074a2ee9a340c
kashishthakur26 <kashishthakur.official.0907@gmail.com>
2024-06-11 19:57:03 +0530

    siggraph model architecture and utils

--- src/eccv16.py
+++ src/eccv16.py
@@ -96,9 +96,11 @@ class ECCVGenerator(BaseColor):
 
         return self.unnormalize_ab(self.upsample4(out_reg))
 
+model_path = r'pytorch_weights/colorization_release_v2-9b330a0b.pth'
+
 def eccv16(pretrained=True):
 	model = ECCVGenerator()
 	if(pretrained):
-		import torch.utils.model_zoo as model_zoo
-		model.load_state_dict(model_zoo.load_url('https://colorizers.s3.us-east-2.amazonaws.com/colorization_release_v2-9b330a0b.pth',map_location='cpu',check_hash=True))
+		state_dict = torch.load(model_path, map_location='cpu')
+		model.load_state_dict(state_dict)
 	return model
